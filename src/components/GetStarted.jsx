c6a0b98274735265a5e3d6c2240b0f808514119a
Elissa <elissadusabe@gmail.com>
2023-05-17 21:51:54 +0200

    added new file

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles  from '../style'
+import  {arrowUp} from '../assets'
+
+const  GetStarted  = () => {
+     return (
+
+          <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] curse-pointer`}>
+
+               <div className={` ${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+
+                    <div className={` ${styles.flexStart} flex-row`}>
+
