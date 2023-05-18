0558fa54b171cf8c83a12e9371ac4f5917053976
Elissa <elissadusabe@gmail.com>
2023-05-18 20:48:27 +0200

    new features

--- src/components/index.js
+++ src/components/index.js
@@ -9,3 +9,21 @@ import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 import Button from "./Button";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+  Button,
+  FeedbackCard,
+  GetStarted
+};
