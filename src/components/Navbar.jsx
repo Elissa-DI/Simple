121a3e10de70bb10229492938e43cdcb26ef3cc3
Elissa <elissadusabe@gmail.com>
2023-05-18 20:49:25 +0200

    new features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,3 +10,27 @@ const  Navbar  = () => {
 
      const [toggle, setToggle] =  useState(true)
      return (
+
+          <nav className='w-full flex py-6 justify-between items-center navbar'>
+                <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
+
+                <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+             
+             {navLinks.map((nav, index) => (
+                  <li
+                   key={nav.id} 
+                  className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'} text-white `}>
+                       <a href={`#${nav.id}`}> {nav.title}</a>
+
+                  </li>
+             ))}
+        </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle ? close : menu}
+           className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle((prev) => !prev) }
+          alt="menu" />
+         
+
+   
