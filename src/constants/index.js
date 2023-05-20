a0f4a05ad1aedaa678f51e03aa5ee1183689df9c
Elissa <elissadusabe@gmail.com>
2023-05-20 21:17:58 +0200

    new feautures

--- src/constants/index.js
+++ src/constants/index.js
@@ -185,3 +185,23 @@ export const socialMedia = [
     icon: linkedin,
     link: "https://www.linkedin.com/",
   },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
